5ee24b2c986c7709fba6af24fd24a149d051b728
tompi04 <126743702+tompi04@users.noreply.github.com>
2023-04-10 02:00:47 +0000

    [Finish] 4-15

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,11 +4,11 @@ class PostImage < ApplicationRecord
 
     has_one_attached :profile_image
 
-    def get_profile_image(width, height)
-      unless profile_image.attached?
-        file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-        profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-      end 
-      profile_image.variant(resize_to_limit: [width, height]).processed
+    def get_image
+      unless image.attached?
+        file_path = Rails.root.join('app/assets/images/no_image.jpg')
+        image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      end
+      image
     end
 end
